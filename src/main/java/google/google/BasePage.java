1f2ba96ad618e5a96e9eb0b1e78c4d9a44c60d1e
karivik <83606575+karivik@users.noreply.github.com>
2021-05-06 10:53:22 -0400

    added a text

--- src/main/java/google/google/BasePage.java
+++ src/main/java/google/google/BasePage.java
@@ -3,7 +3,7 @@ package google.google;
 public class BasePage {
 
 	public static void main(String[] args) {
-		System.out.println("In the basepage");
+		System.out.println("In the basepage testing");
 	}
 
 }
