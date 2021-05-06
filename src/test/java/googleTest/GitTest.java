713faae2f2b0d64de6ddfafc55e066d384f935fc
karivik <karivik111@gmail.com>
2021-05-06 10:56:33 -0400

    Update BasePage.java

--- src/test/java/googleTest/GitTest.java
+++ src/test/java/googleTest/GitTest.java
@@ -3,12 +3,14 @@ package googleTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GitTest {
-
-	public static void main(String[] args) {
+	
+	@Test
+	public void googleTest() {
 		WebDriver driver;
 		
 		WebDriverManager.chromedriver().setup();
